98c4c967499518c58a9e265560c711818db7e6ff
Hengers3012 <hengersrosario.m@gmail.com>
2023-06-09 17:55:40 -0400

    Estableciendo las Rutas de la App..

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Init_Page_App from "./Components/HomePage/Initial_Page";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Init_Page_App} />
+          <Route exact path="/home" />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Components/HomePage/Initial_Page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Init_Page_App() {
+  return (
+    <div>
+      <h1>Henry Food</h1>
+    </div>
+  );
+}

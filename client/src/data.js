ced3c8d7f8fcfc3f65b01979f3db47a31c021b61
Hengers3012 <hengersrosario.m@gmail.com>
2023-06-10 13:30:11 -0400

    Data de 5 Recetas para probar la App

---
+++ client/src/data.js
@@ -0,0 +1,149 @@
+const recipes = [
+  {
+    id: 782585,
+    name: "Cannellini Bean and Asparagus Salad with Mushrooms",
+    summary_of_the_dish:
+      "Cannellini Bean and Asparagus Salad with Mushrooms requires approximately 45 minutes from start to finish. This main course has 482 calories, 31g of protein, and 6g of fat per serving. This gluten free, dairy free, lacto ovo vegetarian, and vegan recipe serves 6 and costs $1.35 per serving. 309 people were impressed by this recipe. Head to the store and pick up grain mustard, sea salt, lemon zest, and a few other things to make it today. It is brought to you by foodandspice.blogspot.com. Taking all factors into account, this recipe earns a spoonacular score of 70%, which is pretty good. Similar recipes are Cannellini Bean Salad, Refreshing Cannellini Bean Salad, and Cannellini-and-Green Bean Salad.",
+    diets: [
+      {
+        name: "gluten free",
+      },
+      {
+        name: "dairy free",
+      },
+      {
+        name: "lacto ovo vegetarian",
+      },
+      {
+        name: "vegan",
+      },
+    ],
+    health_score: 100,
+    image: "https://spoonacular.com/recipeImages/782585-312x231.jpg",
+    createdInDb: false,
+    instructions: [
+      "1- Rinse the cannellini beans and soak for 8 hours or overnight in several inches of water.",
+      "2- Drain and rinse, then transfer to a medium saucepan and cover with fresh water.",
+      "3- Add the curry leaves or bay leaf and bring to a boil. Reduce heat to medium-low, cover, and simmer for 1 hour or until the beans are tender but not falling apart.",
+      "4- Drain and transfer to a large salad bowl.Meanwhile, snap the woody ends off of the asparagus spears and steam the spears for 6 minutes or until just tender but still retaining their crunch.",
+      "5- Transfer to the salad bowl.Now cook the mushrooms.",
+      "6- Heat the oil in a large skillet over high heat. As soon as the oil is hot, drop in the mushrooms and cook, stirring constantly, for 5 minutes or until the mushrooms begin to brown and lose some of their liquid.",
+      "7- Transfer to the bowl with the asparagus.To make the dressing, combine the tarragon, lemon zest, garlic, lemon juice, olive oil and mustard in a small food processor or blender. Process until smooth.",
+      "8- Pour the dressing over the salad, season with salt and pepper, and toss.",
+      "9- Serve at room temperature or chilled.",
+    ],
+  },
+  {
+    id: 716426,
+    name: "Cauliflower, Brown Rice, and Vegetable Fried Rice",
+    summary_of_the_dish:
+      "You can never have too many Chinese recipes, so give Cauliflower, Brown Rice, and Vegetable Fried Rice a try. This recipe serves 8 and costs $1.12 per serving. This hor d'oeuvre has 192 calories, 7g of protein, and 6g of fat per serving. Head to the store and pick up broccoli, sesame seeds, soy sauce, and a few other things to make it today. 3689 people were impressed by this recipe. It is brought to you by fullbellysisters.blogspot.com. It is a good option if you're following a gluten free, dairy free, lacto ovo vegetarian, and vegan diet. From preparation to the plate, this recipe takes roughly 30 minutes. Overall, this recipe earns a spectacular spoonacular score of 100%. If you like this recipe, you might also like recipes such as Cauliflower, Brown Rice, and Vegetable Fried Rice, Cauliflower, Brown Rice, and Vegetable Fried Rice, and Cauliflower, Brown Rice, and Vegetable Fried Rice.",
+    diets: [
+      {
+        name: "gluten free",
+      },
+      {
+        name: "dairy free",
+      },
+      {
+        name: "lacto ovo vegetarian",
+      },
+      {
+        name: "vegan",
+      },
+    ],
+    health_score: 75,
+    image: "https://spoonacular.com/recipeImages/716426-312x231.jpg",
+    createdInDb: false,
+    instructions: [
+      '1- Remove the cauliflower\'s tough stem and reserve for another use. Using a food processor, pulse cauliflower florets until they resemble rice or couscous. You should end up with around four cups of "cauliflower rice."',
+      "2- Heat 1T butter and 1T oil in a large skillet over medium heat.",
+      "3- Add garlic and the white and light green pieces of scallion. Sauté about a minute.",
+      "4- Add the cauliflower to the pan. Stir to coat with oil, then spread out in pan and let sit; you want it cook a bit and to caramelize (get a bit brown), which will bring out the sweetness. After a couple of minutes, stir and spread out again.",
+      "5- Add cold rice (it separates easily, so it won't clump up during cooking), plus the additional grapeseed and coconut oil or butter. Raise heat to medium-high. Toss everything together and, again, spread the mixture out over the whole pan and press a bit into the bottom.",
+      "6- Let it sit for about two minutes—so the rice can get toasted and a little crispy.",
+      "7- Add the peas and broccoli and stir again.",
+      "8- Drizzle soy sauce and toasted sesame oil over rice.Cook for another minute or so and turn off heat.",
+      "9- Add chopped scallion tops and toss.I like to toast some sesame seeds in a dry pan; I sprinkle these and some more raw, chopped scallion over the top of the rice for added flavor and crunch.Season to taste with salt and, if you'd like, more soy sauce. Keep in mind that if you're serving this with something salty and saucy (ie. teriyaki chicken) you may want to hold off on adding too much salt to the fried rice.",
+    ],
+  },
+  {
+    id: 715497,
+    name: "Berry Banana Breakfast Smoothie",
+    summary_of_the_dish:
+      "If you want to add more lacto ovo vegetarian recipes to your recipe box, Berry Banana Breakfast Smoothie might be a recipe you should try. One portion of this dish contains about 21g of protein, 10g of fat, and a total of 457 calories. This recipe serves 1 and costs $2.07 per serving. 689 people have tried and liked this recipe. It works well as a rather inexpensive breakfast. A mixture of banana, graham cracker crumbs, vanilla yogurt, and a handful of other ingredients are all it takes to make this recipe so yummy. From preparation to the plate, this recipe takes roughly 5 minutes. It is brought to you by Pink When. Overall, this recipe earns a super spoonacular score of 99%. Similar recipes include Berry Banana Breakfast Smoothie, Berry Banana Breakfast Smoothie, and Berry Banana Breakfast Smoothie.",
+    diets: [
+      {
+        name: "lacto ovo vegetarian",
+      },
+    ],
+    health_score: 64,
+    image: "https://spoonacular.com/recipeImages/715497-312x231.jpg",
+    createdInDb: false,
+    instructions: [
+      "1- Take some yogurt in your favorite flavor and add 1 container to your blender.",
+      "2- Add in the berries, banana, and soy milk and blend. Top your glass with a few graham cracker crumbs and serve.",
+    ],
+  },
+  {
+    id: 715415,
+    name: "Red Lentil Soup with Chicken and Turnips",
+    summary_of_the_dish:
+      "Red Lentil Soup with Chicken and Turnips might be a good recipe to expand your main course repertoire. This recipe serves 8 and costs $3.0 per serving. One serving contains 477 calories, 27g of protein, and 20g of fat. It is brought to you by Pink When. 1866 people have tried and liked this recipe. It can be enjoyed any time, but it is especially good for Autumn. From preparation to the plate, this recipe takes approximately 55 minutes. It is a good option if you're following a gluten free and dairy free diet. Head to the store and pick up salt and pepper, canned tomatoes, flat leaf parsley, and a few other things to make it today. Overall, this recipe earns a spectacular spoonacular score of 99%. If you like this recipe, you might also like recipes such as Red Lentil and Chicken Soup, Red Lentil and Chicken Soup, and Red-Lentil Soup.",
+    diets: [
+      {
+        name: "gluten free",
+      },
+      {
+        name: "dairy free",
+      },
+    ],
+    health_score: 100,
+    image: "https://spoonacular.com/recipeImages/715415-312x231.jpg",
+    createdInDb: false,
+    instructions: [
+      "1- To a large dutch oven or soup pot, heat the olive oil over medium heat.",
+      "2- Add the onion, carrots and celery and cook for 8-10 minutes or until tender, stirring occasionally.",
+      "3- Add the garlic and cook for an additional 2 minutes, or until fragrant. Season conservatively with a pinch of salt and black pepper.To the pot, add the tomatoes, turnip and red lentils. Stir to combine. Stir in the vegetable stock and increase the heat on the stove to high. Bring the soup to a boil and then reduce to a simmer. Simmer for 20 minutes or until the turnips are tender and the lentils are cooked through.",
+      "4- Add the chicken breast and parsley. Cook for an additional 5 minutes. Adjust seasoning to taste.",
+      "5- Serve the soup immediately garnished with fresh parsley and any additional toppings. Enjoy!",
+    ],
+  },
+  {
+    id: 716406,
+    name: "Asparagus and Pea Soup: Real Convenience Food",
+    summary_of_the_dish:
+      "Asparagus and Pea Soup: Real Convenience Food requires approximately 20 minutes from start to finish. Watching your figure? This gluten free, dairy free, paleolithic, and lacto ovo vegetarian recipe has 217 calories, 11g of protein, and 8g of fat per serving. This recipe serves 2. For $1.78 per serving, this recipe covers 25% of your daily requirements of vitamins and minerals. Autumn will be even more special with this recipe. It works well as a hor d'oeuvre. 207 people have tried and liked this recipe. It is brought to you by fullbellysisters.blogspot.com. A mixture of vegetable broth, evoo, garlic, and a handful of other ingredients are all it takes to make this recipe so yummy. All things considered, we decided this recipe deserves a spoonacular score of 96%. This score is outstanding. Try Asparagus and Pea Soup: Real Convenience Food, Asparagus and Pea Soup: Real Convenience Food, and Asparagus and Pea Soup: Real Convenience Food for similar recipes.",
+    diets: [
+      {
+        name: "gluten free",
+      },
+      {
+        name: "dairy free",
+      },
+      {
+        name: "paleolithic",
+      },
+      {
+        name: "lacto ovo vegetarian",
+      },
+      {
+        name: "primal",
+      },
+      {
+        name: "vegan",
+      },
+    ],
+    health_score: 100,
+    image: "https://spoonacular.com/recipeImages/716406-312x231.jpg",
+    createdInDb: false,
+    instructions: [
+      "1- Chop the garlic and onions.",
+      "2- Saute the onions in the EVOO, adding the garlic after a couple of minutes; cook until the onions are translucent.",
+      "3- Add the whole bag of asparagus and cover everything with the broth. Season with salt and pepper and a pinch of red pepper flakes, if using.Simmer until the asparagus is bright green and tender (if you've thawed the asparagus it will only take a couple of minutes). Turn off the heat and puree using an immersion blender.",
+      "4- Add peas (the heat of the soup will quickly thaw them) and puree until smooth; add more until it reaches the thickness you like.Top with chives and a small dollop of creme fraiche or sour cream or greek yogurt.",
+    ],
+  },
+];
+
+module.export = recipes;

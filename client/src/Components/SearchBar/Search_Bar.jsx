b0e0fbbd39eee3b3d5b53864410fe208df8d7099
Hengers3012 <hengersrosario.m@gmail.com>
2023-06-12 18:04:27 -0400

    Actualizando Componentes del Fron

--- client/src/Components/SearchBar/Search_Bar.jsx
+++ client/src/Components/SearchBar/Search_Bar.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { search_Bar } from "../../Redux/Actions";
 
 import styles from "./Search_Bar.module.css";
 
-export default function SearchBar(props) {
-  console.log(props);
+export default function SearchBar() {
+  const dispatch = useDispatch();
+  const [recipe, setRecipe] = useState("");
+
+  const handleInputChange = (event) => {
+    setRecipe(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className={styles.containerSearchBar}>
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event);
+        }}
+      ></form>
+      <input
+        type="search"
+        onChange={(event) => {
+          handleInputChange(event);
+        }}
+        value={recipe}
+      />
+      <button type="submit">Shearch</button>
+    </div>
+  );
 }

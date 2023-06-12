08a53288171e6e2b661f099143a0855c0b11d8b0
Hengers3012 <hengersrosario.m@gmail.com>
2023-06-12 13:11:18 -0400

    Actualizando Componentes del Fron

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Landing_Page_App from "./Components/Landing_Page/Landing_Page";
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className={styles.App}>
         <Switch>
           <Route exact path="/" component={Landing_Page_App} />
           <Route exact path="/home" />
--- client/src/Components/Landing_Page/Landing_Page.jsx
+++ client/src/Components/Landing_Page/Landing_Page.jsx
@@ -4,7 +4,15 @@ import styles from "./Landing_Page.module.css";
 export default function Landing_Page_App() {
   return (
     <div className={styles.contLanding}>
-      <h1>Henry Food</h1>
+      <div className={styles.routeBar}>
+        <div className={styles.logo}>
+          <img src={"./assets/Image/Food_Logo.png"} alt="" />
+        </div>
+
+        <div className={styles.other}>
+          <h1>Henry Food</h1>
+        </div>
+      </div>
     </div>
   );
 }

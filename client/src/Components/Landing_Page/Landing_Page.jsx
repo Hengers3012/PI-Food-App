e0b0ddd2a70116e239ac9e0ea872bf12ea4cf917
Hengers3012 <hengersrosario.m@gmail.com>
2023-06-16 17:57:30 -0400

    Actualizando el Componente Home.

--- client/src/Components/Landing_Page/Landing_Page.jsx
+++ client/src/Components/Landing_Page/Landing_Page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { SearchBar } from "../SearchBar/Search_Bar";
+import SearchBar from "../SearchBar/Search_Bar";
 import styles from "./Landing_Page.module.css";
 
 export default function LandingPageApp() {
@@ -9,22 +9,25 @@ export default function LandingPageApp() {
     <div className={styles.page}>
       <div className={styles.contLanding}>
         <div className={styles.navBarContainer}>
-          <div>
+          <div className={styles.navBarContPart1}>
             <Link to="/" className={styles.logo}>
               <img src={"./assets/Image/Food_Logo.png"} alt="" />
             </Link>
           </div>
 
-          <div Co>
+          <div className={styles.navBarPart2}>
+            <Link to="/about" className={styles.route}>
+              <h1>Home</h1>
+            </Link>
             <Link to="/about" className={styles.route}>
               <h1>About</h1>
             </Link>
             <Link to="/about" className={styles.route}>
               <h1>FAQ</h1>
             </Link>
-            <Link to="/about" className={styles.route}>
-              <h1>Home</h1>
-            </Link>
+          </div>
+          <div className={styles.searchBarContainer}>
+            <SearchBar className={styles.searchBar} />
           </div>
         </div>
 

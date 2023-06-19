3c40c83ed91add615b5f5abec116458d759a7ff4
Hengers3012 <hengersrosario.m@gmail.com>
2023-06-19 18:01:21 -0400

    Actualizando el Componente Home.

--- client/src/Components/Created_Recipe/Created_Recipe.jsx
+++ client/src/Components/Created_Recipe/Created_Recipe.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
+import { getDiets_Info } from "../../Redux/Actions";
 import NavBarTop from "../Nav_Bar_Top/Nav_Bar_Top";
 import styles from "./Created_Recipe.module.css";
 
 export default function CreateRecipeApp() {
+  const dispatch = useDispatch();
+
+  const diets = useSelector((state) => state.diets);
+
+  useEffect(() => {
+    dispatch(getDiets_Info());
+  }, [dispatch]);
+
+  console.log(diets);
   return (
     <div className={styles.containerPage}>
       <div className={styles.containerNavBarTop}>
@@ -16,52 +27,56 @@ export default function CreateRecipeApp() {
         </div>
       </div>
 
-      <form>
-        <h1>CREA UNA NUEVA RECETA</h1>
+      <div className={styles.labelPage}>
+        <h1>CREANDO UNA NUEVA RECETA</h1>
+      </div>
 
-        <br />
-
+      <form className={styles.containerForm}>
+        <div className={styles.containerNameDiets}>
+          <h3>Nombre</h3>
+          <input type="text" name="name" />
+        </div>
         <div>
-          <div>
-            <h3>Name:</h3>
-            <input type="text" name="name" />
-          </div>
-
-          <div>
-            <h3>Diets:</h3>
-            <input type="checkbox" name="diets" />
-          </div>
-
-          <div>
-            <h3>Summary</h3>
-            <textarea
-              name="summary_of_the_dish"
-              type="text"
-              cols="30"
-              rows="10"
-            />
-          </div>
-
-          <div>
-            <h3>Health Score:</h3>
-            <input type="range" min="0" max="100" name="health_score" />
-          </div>
-
-          <div>
-            <h3>URL Image:</h3>
-            <input type="text" name="image" />
-          </div>
-
-          <div>
-            <h3>Instructions:</h3>
-            <textarea name="instructions" type="text" cols="30" rows="10" />
-          </div>
-
+          <h3>Health Score</h3>
+          <input type="range" min="0" max="100" name="health_score" />
+        </div>
+        <div>
+          <h3>Descripción</h3>
+          <textarea
+            name="summary_of_the_dish"
+            key="summ"
+            type="text"
+            cols="30"
+            rows="10"
+          />
+        </div>
+        <div className={styles.containerCheckbox}>
+          <h3>Dietas</h3>
           <br />
-
-          <div>
-            <button type="submit">CREATE RECIPE</button>
-          </div>
+          {diets.map((diet, index) => {
+            return (
+              <label className={styles.dietsLabel}>
+                <input
+                  type="checkbox"
+                  key={`Diet${index}`}
+                  name={diet.name}
+                  value={diet.name}
+                />
+                {diet.name[0].toUpperCase() + diet.name.slice(1)}
+              </label>
+            );
+          })}
+        </div>
+        <div>
+          <h3>Instrucciones</h3>
+          <textarea name="instructions" type="text" cols="30" rows="10" />
+        </div>
+        <div>
+          <h3>Image</h3>
+          <input type="text" name="image" />
+        </div>
+        <div>
+          <button type="submit">CREATE RECIPE</button>
         </div>
       </form>
     </div>

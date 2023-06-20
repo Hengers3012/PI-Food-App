76714677fcb680f95dc518c8e4ea6e15989994ed
Hengers3012 <hengersrosario.m@gmail.com>
2023-06-20 17:51:37 -0400

    Actualizando el Componente Create_Reciped

--- client/src/Components/Created_Recipe/Created_Recipe.jsx
+++ client/src/Components/Created_Recipe/Created_Recipe.jsx
@@ -1,44 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
 import { getDiets_Info, post_Recipe } from "../../Redux/Actions";
 import NavBarTop from "../Nav_Bar_Top/Nav_Bar_Top";
+
 import styles from "./Created_Recipe.module.css";
 
 export default function CreateRecipeApp() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const history = useHistory();
 
   const diets = useSelector((state) => state.diets);
+  const allRecipes = useSelector((state) => state.allRecipes);
+
+  let recipesNames = allRecipes.map((el) => el.name);
+
   const [recipeError, setRecipeErrors] = useState({});
   const [recipeData, setRecipeData] = useState({
     name: "",
     summary_of_the_dish: "",
-    health_score: 0,
+    health_score: 50,
     instructions: [""],
     image: "",
     diet: [],
   });
 
-  function validate() {
+  function validate(newRecipe) {
     let recipeError = {};
 
     if (recipeData.name.length !== "") {
       recipeError.name = "Se requiere un nombre";
-    } else if (recipeData.name.length > 100) {
+    } else if (recipeData.name.length > 255) {
       recipeError.name =
         "El nombre de la receta no debe sobrepasar los 100 caracteres.";
     } else if (recipeData.summary_of_the_dish.length !== "") {
       recipeError.summary_of_the_dish =
         "Se requiere una descripcción para la Receta";
-    } else if (recipeData.summary_of_the_dish.length > 200) {
+    } else if (recipeData.summary_of_the_dish.length > 255) {
       recipeError.summary_of_the_dish =
         "La descripción de la receta no debe sobrepasar los 200 caracteres.";
     } else if (recipeData.instructions.length !== "") {
       recipeError.instructions =
         "Se requieren las instruccíones para la receta.";
-    } else if (recipeData.instructions.length > 200) {
+    } else if (recipeData.instructions.length > 255) {
       recipeError.instructions =
         "Las instrucciones no deben sobrepasar los 200 caracetee";
     } else if (recipeData.image.length !== "") {
@@ -49,45 +54,60 @@ export default function CreateRecipeApp() {
   }
 
   const handleChange = (event) => {
-    const { data, value } = event.target;
+    //const { data, value } = event.target;
     setRecipeData({
       ...recipeData,
-      [data]: value,
+      [event.target.name]: event.target.value,
     });
 
     setRecipeErrors(
       validate({
         ...recipeData,
-        [data]: value,
+        [event.target.name]: event.target.value,
       })
     );
   };
 
   const handleChange_For_Diets = (event) => {
-    const { data } = event.target.checked;
-    const { value } = event.target;
+    // const { data } = event.target.checked;
+    // const { value } = event.target;
 
-    if (data) {
+    if (event.target.checked) {
       setRecipeData({
         ...recipeData,
-        diet: [...recipeData.diet, value],
+        diet: [...recipeData.diet, event.target],
       });
 
       setRecipeErrors(
         validate({
           ...recipeData,
-          diet: [...recipeData.diet, value],
+          diet: [...recipeData.diet, event.target],
         })
       );
     } else {
       setRecipeData({
         ...recipeData,
-        diet: recipeData.diet.filter((element) => element !== value),
+        diet: recipeData.diet.filter((element) => element !== event.target),
       });
     }
   };
 
-  function handleSubmit(event) {}
+  function handleSubmit(event) {
+    dispatch(post_Recipe(recipeData));
+
+    alert("Recipe Created Successfully");
+
+    setRecipeData({
+      name: "",
+      summary_of_the_dish: "",
+      health_score: 50,
+      instructions: [""],
+      image: "",
+      diet: [],
+    });
+
+    history.push("/home");
+  }
 
   useEffect(() => {
     dispatch(getDiets_Info());
@@ -119,24 +139,31 @@ export default function CreateRecipeApp() {
             <div className={styles.containerNameDiets}>
               <h3>Nombre</h3>
               <input
+                autoComplete="off"
                 type="text"
                 name="name"
                 value={recipeData.name}
                 onChange={(event) => handleChange(event)}
+                onPaste={(event) => handleChange(event)}
               />
-              <span>{recipeError.name}</span>
+              {recipeError.name && <p>{recipeError.name}</p>}
             </div>
+
             <div>
               <h3>Descripción</h3>
               <textarea
+                autoComplete="off"
                 name="summary_of_the_dish"
                 type="text"
                 cols="100"
                 rows="10"
                 value={recipeData.summary_of_the_dish}
                 onChange={(event) => handleChange(event)}
+                onPaste={(event) => handleChange(event)}
               />
-              <span>{recipeError.summary_of_the_dish}</span>
+              {recipeError.summary_of_the_dish && (
+                <p>{recipeError.summary_of_the_dish}</p>
+              )}
             </div>
 
             <div>
@@ -148,6 +175,7 @@ export default function CreateRecipeApp() {
                 rows="10"
                 value={recipeData.instructions}
                 onChange={(event) => handleChange(event)}
+                onPaste={(event) => handleChange(event)}
               />
               <span>{recipeError.instructions}</span>
             </div>
@@ -167,26 +195,27 @@ export default function CreateRecipeApp() {
           <div className={styles.containerGridRigth}>
             <div className={styles.containerCheckbox}>
               <h3>Dietas</h3>
-              {diets.map((diet, index) => {
+              {diets.map((diet) => {
                 return (
                   <label className={styles.dietsLabel}>
                     <input
                       type="checkbox"
-                      key={`Diet${index}`}
                       name={diet.name}
                       value={diet.name}
                       onChange={(event) => handleChange_For_Diets(event)}
+                      onPaste={(event) => handleChange_For_Diets(event)}
                     />
                     {diet.name[0].toUpperCase() + diet.name.slice(1)}
                   </label>
                 );
               })}
-              <span>{recipeError.diet}</span>
+              {recipeError.diet && <p>{recipeError.diet}</p>}
             </div>
 
             <div>
               <h3>Health Score</h3>
               <input
+                autoComplete="off"
                 name="health_score"
                 type="range"
                 min="0"

464b76e07717ce2f7072d6acf47cb469df9a91bc
Hengers3012 <hengersrosario.m@gmail.com>
2023-06-28 19:54:33 -0400

    Actualización de componentes React

--- api/src/Data/Data_BDD.js
+++ api/src/Data/Data_BDD.js
@@ -15,20 +15,6 @@ const allDataBDD = async function () {
     console.log(error);
   }
 
-  // const result = data.map((el) => {
-  //   const dietas = el.Diets.map((elm) => elm.name);
-  //   console.log(dietas);
-  //   return {
-  //     id: el.id,
-  //     title: el.title,
-  //     summary: el.summary,
-  //     health_Score: el.health_Score,
-  //     image: el.image,
-  //     steps: el.steps,
-  //     diets: dietas,
-  //   };
-  // });
-
   // // console.log(data, "esto es la base de datos");
   // return result;
 };

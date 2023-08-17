b51308829458f57d08ca9fc7321fb9f9d4648912
Hengers3012 <hengersrosario.m@gmail.com>
2023-08-16 23:11:20 -0400

    Update Cargando Comonent

--- client/src/Components/Cargando/Cargando.jsx
+++ client/src/Components/Cargando/Cargando.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import styles from "./Cargando.module.css";
 
 export default function CargandoPage() {
+  const text = "LOADING...";
+  const letters = text.split("");
+
   return (
-    <div className={styles.containerCargando}>
-      <h2>LOADING...</h2>
+    <div className={styles.container}>
+      <div className={styles.loading}>
+        {letters.map((letter, index) => (
+          <span
+            key={index}
+            className={styles.letter}
+            style={{ animationDelay: `${index * 0.1}s` }}
+          >
+            {letter}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
